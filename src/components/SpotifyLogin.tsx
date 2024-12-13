8f0c0c1d72534b59220b2253caac4c2a636ef5ed
Noel Schneider <noelwschneider@gmail.com>
2024-12-12 20:11:23 -0600

    add spotify auth handling

---
+++ src/components/SpotifyLogin.tsx
@@ -0,0 +1,19 @@
+import api from '../api/api';
+
+
+export default function SpotifyLogin() {
+    const handleSpotifyLogin = async () => {
+      try {
+        window.location.href = `${api.defaults.baseURL}/spotify/login`;
+      } catch (err) {
+        console.error('Error during Spotify login:', err);
+      };
+    };
+  
+    return (
+      <div>
+        <h1>Spotify Authentication</h1>
+        <button onClick={handleSpotifyLogin}>Login with Spotify</button>
+      </div>
+    );
+};
---
+++ src/components/SpotifyLoginCallback.tsx
@@ -0,0 +1,30 @@
+import { useEffect } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
+import api from '../api/api';
+
+
+export default function SpotifyLoginCallback() {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const queryParams = new URLSearchParams(location.search);
+    const code = queryParams.get('code');
+
+    if (code) {
+      // IIFE allows use of async in useEffect
+      (async () => {
+        try {
+          await api.get(`/spotify/callback?code=${code}`);
+          navigate('/home');
+        } catch (err) {
+          console.error('Error during Spotify callback handling:', err);
+        }
+      })();
+    } else {
+      console.error('Authorization code is missing from query parameters.');
+    }
+  }, [location.search, navigate]);
+
+  return <div>Processing Spotify Login...</div>;
+};
