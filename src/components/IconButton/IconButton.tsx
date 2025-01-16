a35b8b5bccbfb3449d3356c747d533f26f84d718
Noel Schneider <noelwschneider@gmail.com>
2025-01-16 13:19:24 -0600

    add IconButton component

---
+++ src/components/IconButton/IconButton.tsx
@@ -0,0 +1,15 @@
+import styles from './IconButton.module.css';
+
+
+export default function IconButton({ icon, fn, color='primary' }) {
+    
+    return (
+        <div className={styles['icon-button-wrapper']}>
+            <button className={styles['icon-button'] + ` text-${color}`}
+                onClick={fn}
+            >
+                { icon }
+            </button>
+        </div>
+    )
+};
