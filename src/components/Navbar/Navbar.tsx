68c038c0aa3f14d07447968112ef1dada5705197
Noel Schneider <noelwschneider@gmail.com>
2025-01-16 13:22:36 -0600

    add logout button to navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,8 +1,21 @@
 import { Link } from 'react-router-dom';
 import styles from './Navbar.module.css';
+import api from '../../api/_api';
+import { useNavigate } from 'react-router-dom';
 
 
 export default function Navbar() {
+    const navigate = useNavigate();
+    
+    const handleLogout = async () => {
+        try {
+          await api.auth.logout();
+          navigate('/login');
+        } catch (err) {
+          console.error('Logout failed', err);
+        }
+    };
+
     return (
         <nav className={styles["navbar"]}>
             <div className={styles["navbar-container"]}>
@@ -21,6 +34,8 @@ export default function Navbar() {
                         <Link to="/dummy3" className={styles["navbar-link"]}>Dummy Menu 3</Link>
                     </li>
                 </ul>
+
+                <button onClick={handleLogout}>Logout</button>
             </div>
         </nav>
     );
