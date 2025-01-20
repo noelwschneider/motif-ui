2f0d894a44af24c345f7eeee3620ff220e7f28dc
Noel Schneider <noelwschneider@gmail.com>
2025-01-20 16:52:24 -0600

    add initial UI for ArtistProfile page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,19 +1,12 @@
-import { useNavigate } from 'react-router-dom';
-import api from '../api/_api.js';
-import Searchbar from './Search/Searchbar.jsx';
-import CatalogsOverview from './Catalogs/CatalogsOverview.jsx';
+import ArtistProfile from './ArtistProfile/ArtistProfile.jsx';
 
 
 export default function Home() {
-  const navigate = useNavigate();
+  
 
   return (
     <div>
-      <h1>Homepage</h1>
-      <p>This is a protected route. Only authenticated users can see this.</p>
-
-      <Searchbar />
-      <CatalogsOverview />
+      <ArtistProfile />
     </div>
   );
 };
