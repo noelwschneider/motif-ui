7da9c845ba6a8fb98cad4d85867796980e01f389
Noel Schneider <noelwschneider@gmail.com>
2025-01-28 14:18:09 -0600

    minor updates to api utilities
    add early return condition to artistProfile method
    
    add boilerplate for reviews utility
    
    minor formatting improvements

---
+++ src/api/review.js
@@ -0,0 +1,15 @@
+export default function addReviews(api) {
+    const urlPrefix = '/reviews';
+
+    api.reviews = {
+        create: async () => {},
+
+        delete: async () => {},
+        
+        edit: async () => {},
+
+        update: async () => {},
+
+
+    }
+};
