c5050660d3d0193eee2265cac132c75f35d0d7f7
Noel Schneider <noelwschneider@gmail.com>
2025-01-28 14:24:28 -0600

    refactor Login component and relevant user logic in navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,18 +1,24 @@
 import { Link } from 'react-router-dom';
 import styles from './Navbar.module.css';
 import api from '../../api/_api';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { useContext } from 'react';
+import { UserContext } from '../../App';
 
 
 export default function Navbar() {
+    const location = useLocation();
     const navigate = useNavigate();
-    
+    const { user, setUser } = useContext(UserContext);
+
     const handleLogout = async () => {
         try {
-          await api.auth.logout();
-          navigate('/login');
+            await api.auth.logout();
+            navigate('/');
         } catch (err) {
-          console.error('Logout failed', err);
+            console.error('Logout failed', err);
+        } finally {
+            setUser(null);
         }
     };
 
@@ -25,17 +31,25 @@ export default function Navbar() {
 
                 <ul className={styles["navbar-menu"]}>
                     <li className={styles["navbar-item"]}>
-                        <Link to="/dummy1" className={styles["navbar-link"]}>Dummy Menu 1</Link>
+                        <Link to="/dummy1" className={styles["navbar-link"]}>Explore</Link>
                     </li>
                     <li className={styles["navbar-item"]}>
-                        <Link to="/dummy2" className={styles["navbar-link"]}>Dummy Menu 2</Link>
-                    </li>
-                    <li className={styles["navbar-item"]}>
-                        <Link to="/dummy3" className={styles["navbar-link"]}>Dummy Menu 3</Link>
+                        <Link to="/dummy2" className={styles["navbar-link"]}>User</Link>
                     </li>
                 </ul>
 
-                <button onClick={handleLogout}>Logout</button>
+                { user ? 
+                    <div>
+                        <p>{user.username}</p>
+                        <button onClick={handleLogout}>Logout</button>
+                    </div>
+                    : (
+                        <Link to="/login" state={{ loginRedirect: `${location.pathname}?${location.search}` }}>
+                            <button>Login</button>
+                        </Link>
+                    )
+                }
+                
             </div>
         </nav>
     );
