453f6c3e764657df7a1db05afdf082b1db7dc08f
Noel Schneider <noelwschneider@gmail.com>
2025-01-28 14:23:28 -0600

    add CatalogModal component

--- src/components/Catalogs/CatalogsOverview.jsx
+++ src/components/Catalogs/CatalogsOverview.jsx
@@ -1,13 +1,15 @@
 import { useState, useEffect } from 'react';
 import { Edit, Trash2 } from 'react-feather';
-import IconButton from '../IconButton/IconButton';
+import IconButton from '../Icon/IconButton';
 import api from '../../api/_api';
 import styles from './Catalogs.module.css';
+import CatalogModal from './CatalogModal';
 
 
 export default function CatalogsOverview() {
     const [catalogs, setCatalogs] = useState([]);
-    const [newCatalog, setNewCatalog] = useState({ name: '', description: '' });
+    const [modalOpen, setModalOpen] = useState(false);
+    const [modalData, setModalData] = useState(null);
     const [error, setError] = useState('');
     const [loading, setLoading] = useState(false);
 
@@ -28,34 +30,6 @@ export default function CatalogsOverview() {
         fetchCatalogs();
     }, []);
 
-    const handleCreateCatalog = async (e) => {
-        e.preventDefault();
-        if (!newCatalog.name) {
-            setError('Name is required');
-            return;
-        }
-        try {
-            await api.catalogs.create({
-                description: newCatalog.description,
-                name: newCatalog.name,
-                isPrivate: false
-            });
-            setNewCatalog({ name: '', description: '' });
-            fetchCatalogs();
-        } catch (err) {
-            setError('Failed to create catalog', err);
-        }
-    };
-
-    const handleUpdateCatalog = async (catalog) => {
-        // todo: add form to edit details
-        // todo: refactor api.catalogs.update to just take the whole catalog object and handle the properties internally
-
-        // const { id, description, name, isPrivate } = catalog;
-        // await api.catalogs.update(id, { description, name, isPrivate });
-        // fetchCatalogs();
-    };
-
     const handleDeleteCatalog = async (id) => {
         try { 
             await api.catalogs.delete(id);
@@ -65,10 +39,16 @@ export default function CatalogsOverview() {
         }
     };
 
+    const handleModal = (initialData) => {
+        setModalData(initialData);
+        setModalOpen(true);
+    };
+
     return (
         <div className={styles['catalogs-container']}>
             <h1>My Catalogs</h1>
 
+            <button onClick={() => handleModal(null)}>New Catalog</button>
             {loading ? (
                 <p>Loading...</p>
             ) : error ? (
@@ -82,7 +62,13 @@ export default function CatalogsOverview() {
                                 <p>{catalog.description}</p>
                             </div>
                             <div className={styles['catalog-actions']}>
-                                <IconButton icon={<Edit />} />
+                                <IconButton icon={<Edit />} 
+                                    fn={() => handleModal({
+                                        name: catalog.name,
+                                        description: catalog.description,
+                                        isPrivate: catalog.isPrivate
+                                    })}
+                                />
                                 <IconButton icon={<Trash2 />} 
                                     fn={() => handleDeleteCatalog(catalog.id)}
                                     color='danger'
@@ -93,23 +79,15 @@ export default function CatalogsOverview() {
                 </ul>
             )}
 
-            {/* todo: extract this form to a modal */}
-            <form onSubmit={handleCreateCatalog} className={styles['catalog-form']}>
-                <h2>Create New Catalog</h2>
-                <input
-                    type="text"
-                    placeholder="Catalog Name"
-                    value={newCatalog.name}
-                    onChange={(e) => setNewCatalog({ ...newCatalog, name: e.target.value })}
-                    required
-                />
-                <textarea
-                    placeholder="Description"
-                    value={newCatalog.description}
-                    onChange={(e) => setNewCatalog({ ...newCatalog, description: e.target.value })}
-                />
-                <button type="submit">Create Catalog</button>
-            </form>
+            <CatalogModal 
+                isOpen={modalOpen}
+                onClose={() => {
+                    setModalOpen(false);
+                    setModalData(null);
+                    fetchCatalogs();
+                }}
+                initialData={modalData}
+            />
         </div>
     );
 };
