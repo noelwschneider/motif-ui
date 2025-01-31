ab390c9e8fedbb77cbf0bf05481aeec3b802b7b5
Noel Schneider <noelwschneider@gmail.com>
2025-01-31 15:39:37 -0600

    refactor axios interceptors
    move to main api file
    
    reincorporate logic for handling expired access tokens

--- src/api/_api.js
+++ src/api/_api.js
@@ -1,10 +1,12 @@
 import axios from 'axios';
+import { getCookie } from '../util';
 import addAuth from './auth';
 import addCatalogs from './catalog';
 import addReviews from './review';
 import addSpotify from './spotify';
 
 
+// api instance
 const api = axios.create({
   baseURL: 'http://127.0.0.1:5174/api', // todo: env variables
   timeout: 10000,
@@ -15,10 +17,50 @@ const api = axios.create({
   },
 });
 
+
+// modules
 addAuth(api);
 addCatalogs(api);
 addReviews(api);
 addSpotify(api);
+
+
+// interceptors
+api.interceptors.response.use(
+  (response) => {
+    try {
+      const newCsrfToken = getCookie("csrf_access_token");
+      api.defaults.headers["X-CSRF-TOKEN"] = newCsrfToken;
+      return response;
+    } catch (error) {
+      console.error("Failed to refresh token:", error);
+      throw error;
+    };
+  },
+  async (error) => {
+    if (error.response?.status === 401) {
+      const authCookie = getCookie("access_token_cookie");
+      if (authCookie) {
+        console.warn("Session expired. Logging out...");
+        try {
+          await api.auth.logout();
+        } catch (logoutError) {
+          console.error("Error during logout:", logoutError);
+        }
+        window.location.href = "/login?sessionExpired=true";
+      }
+    };
+    return Promise.reject(error);
+  }
+);
+
+api.interceptors.request.use((config) => {
+  const csrfToken = getCookie('csrf_access_token');
+  if (csrfToken) {
+      config.headers['X-CSRF-TOKEN'] = csrfToken;
+  };
+  return config;
+});
 
 
 export default api;
--- src/util.js
+++ src/util.js
@@ -3,3 +3,11 @@ export function formatDuration(ms) {
     const seconds = ((ms % 60000) / 1000).toFixed(0);
     return `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
 };
+
+
+export function getCookie(name) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(';').shift();
+    return null;
+};
