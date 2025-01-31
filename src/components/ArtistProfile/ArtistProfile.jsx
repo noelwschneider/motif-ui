5c2ac1768e98ccb677629bd745f3465c2643b09e
Noel Schneider <noelwschneider@gmail.com>
2025-01-31 16:20:00 -0600

    refactor Explore and ArtistProfile components
    refactor artistProfile object to expect separate albums, singles, compilations properties
    
    improve ui appearance
    
    add functionality to open ReviewModal if user wants to add a review
    
    refactor handleClick helper to manage interactions between Search and ArtistProfile components

--- src/components/ArtistProfile/ArtistProfile.jsx
+++ src/components/ArtistProfile/ArtistProfile.jsx
@@ -1,6 +1,8 @@
 import styles from './ArtistProfile.module.css';
-import { PlusCircle } from 'react-feather';
-import { useEffect, useState } from 'react';
+import { Plus } from 'react-feather';
+import { useState } from 'react';
+import ReviewModal from '../ReviewModal/ReviewModal';
+import AlbumList from './AlbumList';
 
 
 // todo: support multiple artists by allowing user to toggle between them. Load both discographies / reviews / etc.
@@ -12,107 +14,89 @@ import { useEffect, useState } from 'react';
     // stretch: add alternate track view
     // stretch: open in spotify
     // stretch: suspense/placeholder UI
-export default function ArtistProfile({ itemId, setItemId, artistProfile }) {
-    const [expandedAlbumIndex, setExpandedAlbumIndex] = useState(null);
-
-    const handleItemClick = (selectedItemId, albumIndex) => {
-        if (selectedItemId === itemId) return;
-        if (selectedItemId === artistProfile.spotifyId) setExpandedAlbumIndex(null);
-        setItemId(selectedItemId);
-    };
+export default function ArtistProfile({ 
+    albumIndex,
+    item,
+    artistProfile,
+    handleClick 
+}) {
+    const [modalOpen, setModalOpen] = useState(false);
+    const [showAlbums, setShowAlbums] = useState('albums');
     
-
     return (
-        <div className={styles['artist-profile']}>
-            { artistProfile && 
-                <>
-                    <div className={styles["artist-data-container"]}>
-                        <h1 className={ styles['artist-name'] +
-                                (itemId === artistProfile.spotifyId ? ' ' + styles['active'] : '')
-                            }
-                            onClick={() => handleItemClick(artistProfile?.spotifyId)}
-                        >
-                            {artistProfile?.title}
-                        </h1>
+        <div className={styles['artist-profile-container']}>
+            <div className={styles["artist-data-container"]}>
+                <h1 className={ styles['artist-name'] + 
+                    ' clickable' 
+                    + (item?.spotifyId === artistProfile?.spotifyId ? ' text-primary' : '')}
+                    onClick={() => { handleClick(artistProfile?.spotifyId) }}
+                >
+                    {artistProfile?.title}
+                </h1>
 
-                        {artistProfile?.albums &&
-                            <div className={styles['album-list']}>
-                                {artistProfile?.albums?.sort(
-                                    (a, b) => a.releaseDate > b.releaseDate ? -1 : 1
-                                )?.map((album, albumIndex) => (
-                                    <div className={styles['album-container']}
-                                        key={albumIndex}
-                                    >
-                                        <div className={styles['album-row']}
-                                            onClick={() => {
-                                                handleItemClick(album.spotifyId, albumIndex);
-                                                setExpandedAlbumIndex(albumIndex);  
-                                            }}
-                                        >
-                                            <img src={album.images[0]?.url} height='64' width='64'></img>
-                                            <h2 className={ styles['album-title'] + (
-                                                itemId === album.spotifyId ? 
-                                                    ' ' + styles['active']
-                                                    : ''
-                                            )}>
-                                                {album.title}
-                                            </h2>
-                                            <p className={styles['album-year']}>{album.releaseDate.slice(0,4)}</p>
-                                        </div>
+                {['albums', 'singles', 'compilations'].map((albumType) => (<>
+                    <h1 className={styles['album-type-header'] + ' clickable'}
+                        onClick={() => {setShowAlbums(showAlbums === albumType ? null : albumType)}}
+                    >
+                        {albumType}
+                    </h1>
 
-                                        {expandedAlbumIndex === albumIndex && (
-                                            <div className={styles['track-list']}>
-                                                {album.tracks.sort(
-                                                    (a,b) => a.trackNumber - b.trackNumber
-                                                ).map((track, trackIndex) => (
-                                                    <div className={styles['track-row']}
-                                                        key={trackIndex}
-                                                        onClick={() => handleItemClick(track.spotifyId, albumIndex)}
-                                                    >
-                                                        <span className={styles['track-order']}>
-                                                            {track.trackNumber}
-                                                        </span>
-                                                        <span className={styles['track-title'] + (         
-                                                            itemId === track.spotifyId ? 
-                                                                ' ' + styles['active']
-                                                                : ''
-                                                        )}>
-                                                            {track.title}
-                                                        </span>
-                                                        <span className={styles['track-duration']}>
-                                                            {`${Math.floor(track.durationMs / 60000)}:${Math.floor(track.durationMs % 60000 / 1000)}`}
-                                                        </span>
-                                                    </div>
-                                                ))}
-                                            </div>
-                                        )}
-                                    </div>
-                                ))}
-                            </div>
-                        }
+                    { showAlbums === albumType &&
+                        <AlbumList 
+                            albums={artistProfile[albumType]}
+                            albumIndex={albumIndex}
+                            artistId={artistProfile.spotifyId}
+                            handleClick={handleClick}
+                            activeItemId={item.spotifyId}
+                        />
+                    }
+                </>))}
+            </div>
+
+            <div className={styles['motif-data-container']}>        
+                <img className={styles['item-image']}
+                    src={item.imageUrl}
+                ></img>
+                <h1 className={styles['item-title']}>{item.title}</h1>
+
+                { item.releaseYear && 
+                    <span className={styles['item-year']}>{item.releaseYear}</span>
+                }
+
+                { item.duration && 
+                    <span className={styles['item-duration']}>{item.duration}
+                    </span>
+                }
+                
+                { item.explicit && 
+                    <span className={styles['item-explicit']}>explicit</span>
+                }
+                
+                
+                <Plus className={styles['add-review-button'] + ' clickable'} 
+                    onClick={() => setModalOpen(true)}
+                />
+                <h2>Reviews</h2>
+
+                { artistProfile?.reviews?.[item.spotifyId]?.map((review, ix) => (
+                    <div key={`profile-item-${item.spotifyId}-review-${ix}`}>
+                        <div className="review-id">{review.reviewId}</div>
+                        <div className="spotify-id">{review.spotifyId}</div>
+                        <div className="user-id">{review.userId}</div>
+                        <div className="comment">{review.comment}</div>
+                        <div className="rating">{review.rating}</div>
+                        <div className="created-date">{review.createdDate}</div>
+                        <div className="upvotes">{review.upvotes}</div>
+                        <div>TODO: get username</div>
                     </div>
-
-                    <div className={styles['motif-data-container']}>
-                        { artistProfile?.reviews?.[itemId]?.map((review, i) => (
-                            <div key={i}>
-                                <div className="review-id">{review.reviewId}</div>
-                                <div className="spotify-id">{review.spotifyId}</div>
-                                <div className="user-id">{review.userId}</div>
-                                <div className="comment">{review.comment}</div>
-                                <div className="rating">{review.rating}</div>
-                                <div className="created-date">{review.createdDate}</div>
-                                <div className="upvotes">{review.upvotes}</div>
-                            </div>
-                        ))}
-
-                        { !artistProfile?.reviews?.[itemId] &&
-                            <div>
-                                No reviews for this content!
-                            </div>
-                        }
-                    </div>
-                </> 
-            }
+                ))}
+            </div>
+            
+            <ReviewModal 
+                isOpen={modalOpen}
+                onClose={() => setModalOpen(false)}
+                initialData={null}
+            />
         </div>
     );
 };
