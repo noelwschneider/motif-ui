7a572070efc6ca800567b3e67cfc3084ddb9b45a
Noel Schneider <noelwschneider@gmail.com>
2025-02-03 12:22:18 -0600

    conditionally render ArtistProfile page

--- src/components/ArtistProfile/ArtistProfile.jsx
+++ src/components/ArtistProfile/ArtistProfile.jsx
@@ -7,14 +7,10 @@ import { Star } from 'react-feather';
 
 
 // todo: support multiple artists by allowing user to toggle between them. Load both discographies / reviews / etc.
-
-// todo: 
-    // add action buttons
-    // improve UI
-    // stretch: add album sort buttons, object for sorting functions
-    // stretch: add alternate track view
-    // stretch: open in spotify
-    // stretch: suspense/placeholder UI
+// stretch: add album sort buttons, object for sorting functions
+// stretch: add alternate track view
+// stretch: open in spotify
+// stretch: suspense/placeholder UI
 export default function ArtistProfile({ 
     albumIndex,
     artistProfile,
@@ -26,94 +22,102 @@ export default function ArtistProfile({
     
     return (
         <div className={styles['artist-profile-container']}>
-            <div className={styles["artist-data-container"]}>
-                <h1 className={ styles['artist-name'] + 
-                    ' clickable' 
-                    + (item?.spotifyId === artistProfile?.spotifyId ? ' text-primary' : '')}
-                    onClick={() => { handleClick(artistProfile?.spotifyId) }}
-                >
-                    {artistProfile?.title}
-                </h1>
-
-                {['albums', 'singles', 'compilations'].map((albumType) => (<>
-                    <h1 className={styles['album-type-header'] + ' clickable'}
-                        onClick={() => {setShowAlbums(showAlbums === albumType ? null : albumType)}}
+            {item && <>
+                <div className={styles["artist-data-container"]}>
+                    <h1 className={ styles['artist-name'] + 
+                        ' clickable' 
+                        + (item?.spotifyId === artistProfile?.spotifyId ? ' text-primary' : '')}
+                        onClick={() => { 
+                            setShowAlbums(false);
+                            handleClick(artistProfile?.spotifyId); 
+                        }}
                     >
-                        {albumType}
+                        {artistProfile?.title}
                     </h1>
 
-                    { showAlbums === albumType &&
-                        <AlbumList 
-                            albums={artistProfile[albumType]}
-                            albumIndex={albumIndex}
-                            artistId={artistProfile.spotifyId}
-                            handleClick={handleClick}
-                            activeItemId={item.spotifyId}
-                        />
-                    }
-                </>))}
-            </div>
+                    {['albums', 'singles', 'compilations'].map((albumType) => (<>
+                        <h1 className={styles['album-type-header'] + ' clickable'}
+                            onClick={() => {setShowAlbums(showAlbums === albumType ? null : albumType)}}
+                        >
+                            {albumType} 
+                            <span className={styles['album-type-count']}>
+                                ({artistProfile[albumType].length})
+                            </span>
+                        </h1>
 
-            <div className={styles['motif-data-container']}>        
-                <img className={styles['item-image']}
-                    src={item.imageUrl}
-                ></img>
-
-                <h1 className={styles['item-title']}>
-                    {item.title}
-                </h1>
-                
-                <h2 className={styles['reviews-header']}>
-                    Reviews
-                </h2>
-
-                {/* todo: average review score */}
-                {/* todo: total reviews */}
-
-                <Plus className={styles['add-review-button'] + ' clickable'} 
-                    onClick={() => setModalOpen(true)}
-                />
-
-                { artistProfile?.reviews?.[item.spotifyId]?.map((review, ix) => (
-                    <div className={styles['item-review']}
-                        key={`profile-item-${item.spotifyId}-review-${ix}`}
-                    >
-                        <div className={styles['review-stars-container']}>
-                            { Array.from({length: review.rating}, () => (
-                                <Star className={styles['review-star']} />
-                            ))}
-                            { Array.from({length: 5 - review.rating}, () => (
-                                <Star className={styles['review-star-empty']} />
-                            ))}
-                        </div>
-
-                        <h3 className={styles['review-username'] + ' clickable'}>
-                            username_placeholder
-                        </h3>
-
-                        { review.comment && 
-                            <p className={styles['review-comment']}>
-                                "{review.comment}"
-                            </p>
+                        { showAlbums === albumType &&
+                            <AlbumList 
+                                albums={artistProfile[albumType]}
+                                albumIndex={albumIndex}
+                                artistId={artistProfile.spotifyId}
+                                handleClick={handleClick}
+                                activeItemId={item.spotifyId}
+                            />
                         }
-                       
-                    </div>
-                ))}
-            </div>
+                    </>))}
+                </div>
+
+                <div className={styles['motif-data-container']}>        
+                    <img className={styles['item-image']}
+                        src={item.imageUrl}
+                    ></img>
+
+                    <h1 className={styles['item-title']}>
+                        {item.title}
+                    </h1>
+                    
+                    <h2 className={styles['reviews-header']}>
+                        Reviews
+                    </h2>
+
+                    {/* todo: average review score */}
+                    {/* todo: total reviews */}
+
+                    <Plus className={styles['add-review-button'] + ' clickable'} 
+                        onClick={() => setModalOpen(true)}
+                    />
+
+                    { artistProfile?.reviews?.[item.spotifyId]?.map((review, ix) => (
+                        <div className={styles['item-review']}
+                            key={`profile-item-${item.spotifyId}-review-${ix}`}
+                        >
+                            <div className={styles['review-stars-container']}>
+                                { Array.from({length: review.rating}, () => (
+                                    <Star className={styles['review-star']} />
+                                ))}
+                                { Array.from({length: 5 - review.rating}, () => (
+                                    <Star className={styles['review-star-empty']} />
+                                ))}
+                            </div>
+
+                            <h3 className={styles['review-username'] + ' clickable'}>
+                                {review.displayName}
+                            </h3>
+
+                            { review.comment && 
+                                <p className={styles['review-comment']}>
+                                    "{review.comment}"
+                                </p>
+                            }
+                        
+                        </div>
+                    ))}
+                </div>
             
-            {/* todo: check whether user already has a review saved for the item, set data prop accordingly */}
-            { modalOpen &&
-                <ReviewModal 
-                    isOpen={modalOpen}
-                    onClose={() => setModalOpen(false)}
-                    data={{
-                        spotifyArtistId: item.spotifyArtistId,
-                        spotifyId: item.spotifyId,
-                        title: item.title,
-                    }}
-                    onSubmit={() => handleClick(item.spotifyArtistId, item.spotifyId, albumIndex)}
-                />
-            } 
+                {/* todo: check whether user already has a review saved for the item, set data prop accordingly */}
+                { modalOpen &&
+                    <ReviewModal 
+                        isOpen={modalOpen}
+                        onClose={() => setModalOpen(false)}
+                        data={{
+                            spotifyArtistId: item.spotifyArtistId,
+                            spotifyId: item.spotifyId,
+                            title: item.title,
+                        }}
+                        onSubmit={() => handleClick(item.spotifyArtistId, item.spotifyId, albumIndex)}
+                    />
+                } 
+            </>}
         </div>
     );
 };
