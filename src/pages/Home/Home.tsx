57005e7e4062730bf527e53aa7fd8b55140c6e61
Noel Schneider <noelwschneider@gmail.com>
2025-02-17 20:02:58 -0600

    refactor app search logic
    move artistProfile and reviews fetch logic to utilities
    
    simplify component for display search results
    
    use global search provider for search result data

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,25 +1,21 @@
 import styles from './Home.module.css';
 import ArtistProfile from 'components/ArtistProfile/ArtistProfile';
 import Search from 'components/Search/Search';
-import api from 'app/api';
 import { useState } from 'react';
-import { formatDuration } from 'utils/utils';
+import { fetchArtist, getAlbumIndex, getSelectedItemData, } from 'utils/utils';
 
 
 export default function Home() {
     const [artistProfiles, setArtistProfiles] = useState({});
     const [itemData, setItemData] = useState(null);
     const [albumIndex, setAlbumIndex] = useState<number | null>(null);
-    const [searchOpen, setSearchOpen] = useState<boolean>(true);
 
     // todo: accept tracks w/ multiple artists
     const handleClick = async (selectedArtistId: string, selectedItemId?: string, ix?: number) => {
         const itemId = selectedItemId || selectedArtistId;
-        const artistProfile = await fetchArtistProfile(selectedArtistId);
-
+        const artistProfile = await fetchArtist(selectedArtistId);
+        
         if (!artistProfile) return;
-
-        artistProfile.reviews = await fetchReviews(selectedArtistId);
 
         setArtistProfiles(prev => ({
             ...prev,
@@ -29,40 +25,11 @@ export default function Home() {
         const albumIx = ix ?? getAlbumIndex(itemId, artistProfile);
         setAlbumIndex(albumIx);
         setItemData(getSelectedItemData(itemId, albumIx, artistProfile));
-        setSearchOpen(false);
-    };
-
-    const fetchReviews = async (artistId: string) => {
-        try {
-            const reviews = await api.reviews.getArtist(artistId);
-            return reviews?.data;
-        } catch (err) {
-            console.error(err);
-            return null;
-        }
-    };
-
-    const fetchArtistProfile = async (artistId: string) => {
-        if (artistProfiles[artistId]) {
-            return artistProfiles[artistId];
-        }
-
-        try {
-            const artistProfileResponse = await api.spotify.artistProfile(artistId);
-            return artistProfileResponse?.data ?? null;
-        } catch (err) {
-            console.error(err);
-            return null;
-        }
     };
 
     return (
         <div className={styles['explore-container']}>
-            <Search
-                handleSearchClick={handleClick}
-                searchOpen={searchOpen}
-                setSearchOpen={setSearchOpen}
-            />
+            <Search handleSearchClick={handleClick} />
 
             <ArtistProfile
                 artistProfile={artistProfiles[itemData?.spotifyArtistId || '']}
@@ -72,82 +39,4 @@ export default function Home() {
             />
         </div>
     );
-};
-
-// HELPERS
-function getSelectedItemData(
-    itemId: string,
-    albumIndex: number | null,
-    artistProfile,
-) {
-    if (!artistProfile) return null;
-
-    if (itemId === artistProfile.spotifyId) {
-        return {
-            spotifyId: artistProfile.spotifyId,
-            spotifyArtistId: artistProfile.spotifyId,
-            title: artistProfile.title,
-            popularity: artistProfile.popularity,
-            imageUrl: artistProfile.images[1]?.url ?? '',
-            releaseYear: null,
-            duration: null,
-            explicit: null,
-            isPlayable: null,
-            albumType: null,
-        };
-    }
-
-    if (albumIndex !== null && itemId === artistProfile.albums[albumIndex]?.spotifyId) {
-        const albumObj = artistProfile.albums[albumIndex];
-        return {
-            spotifyId: albumObj.spotifyId,
-            spotifyArtistId: artistProfile.spotifyId,
-            title: albumObj.title,
-            popularity: albumObj.popularity,
-            imageUrl: albumObj.images[1]?.url ?? '',
-            releaseYear: albumObj.releaseDate.slice(0, 4),
-            duration: formatDuration(albumObj.tracks.reduce((acc, cur) => acc + cur.durationMs, 0)),
-            explicit: null,
-            isPlayable: null,
-            albumType: albumObj.albumType,
-        };
-    }
-
-    if (albumIndex !== null) {
-        const albumObj = artistProfile.albums[albumIndex];
-        for (const track of albumObj.tracks) {
-            if (track.spotifyId === itemId) {
-                return {
-                    spotifyId: track.spotifyId,
-                    spotifyArtistId: artistProfile.spotifyId,
-                    title: track.title,
-                    popularity: 'todo',
-                    imageUrl: albumObj.images[1]?.url ?? '',
-                    releaseYear: albumObj.releaseDate.slice(0, 4),
-                    duration: formatDuration(track.durationMs),
-                    explicit: track.explicit,
-                    isPlayable: track.isPlayable,
-                    albumType: albumObj.albumType,
-                };
-            }
-        }
-    }
-
-    return null;
-};
-
-
-function getAlbumIndex(itemId: string, artistProfile): number | null {
-    if (!artistProfile) return null;
-    if (itemId === artistProfile.spotifyId) return null;
-
-    let count = 0;
-    for (const album of artistProfile.albums || []) {
-        if (itemId === album.spotifyId) return count;
-        for (const track of album.tracks || []) {
-            if (itemId === track.spotifyId) return count;
-        }
-        count++;
-    }
-    return null;
 };
